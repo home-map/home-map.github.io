64455e5466a3faaf054e5feb564dd3ed5dc1ae3c
Daniel <niel@apx_managed_dnf.vanilla>
2023-09-23 20:37:54 -0300

    header creation: added to logo and button; creation of the public folder; creation of main: added title and paragraph; favicon and title changes

--- src/header/header.js
+++ src/header/header.js
@@ -1,9 +1,11 @@
 export const header = `
 <header>
-    <div class="">
-    <img>  
-    <h1>Home Map</h1>
-    <bottom></bottom>
+    <div class="pt-[82px] pl-[86px] pr-[86px] pb-[214px] flex justify-between items-center text-center">
+        <div class="flex gap-[10px] items-center">
+            <img src="/logo-home-map.svg" class="w-10 h-10 ">  
+            <h1 class="text-zinc-200 font-normal font-sans text-4xl tracking-[0.96px]">Home Map</h1>
+        </div>
+        <bottom class="cursor-pointer text-2xl font-medium text-[#00AB47] bg-trasnparent border border-[#00AB47] hover:bg-[#00AB47] hover:text-slate-50 rounded-lg pt-[5px] pb-[7px] pl-[42px] pr-[42px]">login</bottom>
     </div>
 </header>
 `
